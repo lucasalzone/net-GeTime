3c857483ec3d52b6f4616b954f94f9a2a9bb4528
Alessandro Fabiani <corsi.alfasoft@gmail.com>
2018-03-29 16:02:45 +0200

    Aggiunto HF procedure Aggiunto CompilaHF SU Alex.cs

--- LibGeTime/SqlProcedure.sql
+++ LibGeTime/SqlProcedure.sql
@@ -1,6 +1,4 @@
-﻿
-
-create procedure SearchCommessa
+﻿create procedure SearchCommessa
 	@nomeCommessa nvarchar,
 	@idUtente int
 as
@@ -24,11 +22,36 @@ create Procedure AddHM
 @Giorno date,
 @Utenti int
 as
-
 declare @id int ;
 set @id = (Select top 1 id from Giorni where giorno=@giorno and TipoOre = 4 )
 	if id is null 
 	Insert into giorni (TipoOre,Ore,Giorno,idUtenti) values (4,@ore,@Giorno,@Utenti) 
+	else
+	Update giorni set Ore=@ore where id=@id;
+go;
+
+create procedure AddHF
+@Ore int,
+@Giorno date,
+@Utenti int
+as
+declare @id int ;
+set @id = (Select top 1 id from Giorni where giorno=@giorno and TipoOre = 2 )
+	if id is null 
+	Insert into giorni (TipoOre,Ore,Giorno,idUtenti) values (2,@ore,@Giorno,@Utenti) 
+	else
+	Update giorni set Ore=@ore where id=@id;
+go;
+
+create procedure AddHP
+@Ore int,
+@Giorno date,
+@Utenti int
+as
+declare @id int ;
+set @id = (Select top 1 id from Giorni where giorno=@giorno and TipoOre = 3 )
+	if id is null 
+	Insert into giorni (TipoOre,Ore,Giorno,idUtenti) values (3,@ore,@Giorno,@Utenti) 
 	else
 	Update giorni set Ore=@ore where id=@id;
 go;
